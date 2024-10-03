233bf342b3a1f42ed5325aa421b9e4605aa6815f
Arash-hqz <ghaffariarash369@gmail.com>
2024-10-03 14:43:22 +0330

    www

--- src/screens/CourseDetail/Accordion.jsx
+++ src/screens/CourseDetail/Accordion.jsx
@@ -4,7 +4,7 @@ const Accordion = () => {
   return (
     <>
     
-<div id="accordion-color" data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
+<div id="accordion-color " data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
   <h2 id="accordion-color-heading-1">
     <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
       <span>What is Flowbite?</span>
