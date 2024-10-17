9520664b8b3087de07210426cb9d759fbbce911f
Arash-hqz <ghaffariarash369@gmail.com>
2024-10-17 15:21:01 +0330

    update font

--- src/screens/News/News.jsx
+++ src/screens/News/News.jsx
@@ -1,173 +1,155 @@
-import React from 'react'
-import images from '../../assets/img/News'
+import React from "react";
+import images from "../../assets/img/News";
 const News = () => {
   return (
     <>
-      <div className="w-[100%] h-[1853px] border flex flex-wrap relative bg-NewsBg bg-[#F9F9F9] font-['BYekan']">
+      <div className="w-[100%] h-[1853px] border flex flex-wrap relative bg-NewsBg bg-[#F9F9F9] font-['BYekan1']">
+        <div className="border-2 border-red-800 w-[76%] h-[500px] mr-auto ml-auto ">
+          <div
+            dir="rtl"
+            className=" w-[98%] h-[272px] flex ml-auto mt-[74px] rounded-tr-[80px] rounded-bl-[80px] rounded-tl-[10px] rounded-br-[10px] bg-[#EBF9F982] shadow-[0_1px_2px_0px_rgba(0,0,0,0.4)] "
+          >
+            <div className=" w-[55%] h-[100%] border-2 relative ">
+              <div className=" w-[300px] h-[86px] flex flex-wrap absolute right-[33px] top-[40px] border-2 ">
+                <div className=" w-[14px] h-[14px] rounded-full bg-[#01CEC9] relative top-5   "></div>
 
-          <div className="border-2 border-red-800 w-[76%] h-[500px] mr-auto ml-auto ">
-               
-
-               <div dir='rtl' className=" w-[98%] h-[272px] flex ml-auto mt-[74px] rounded-tr-[80px] rounded-bl-[80px] rounded-tl-[10px] rounded-br-[10px] bg-[#EBF9F982] shadow-[0_1px_2px_0px_rgba(0,0,0,0.4)] ">
-
-
-
-                   <div className=" w-[55%] h-[100%] border-2 relative ">
-                       <div className=" w-[300px] h-[86px] flex flex-wrap absolute right-[33px] top-[40px] border-2 ">
-                           
-                           <div className=" w-[14px] h-[14px] rounded-full bg-[#01CEC9] relative top-5   "></div>
-                           
-                           <div className=" w-[80%] h-[50%] font-bold text-[35px] leading-[50px] text-[#00000] mr-3 ">
-                           اخبار و مقالات
-                           </div>
-                           
-                           <div className=" w-[60%] h-[50%] font-normal text-[20px] leading-[45px] text-[#AAAAAA] mr-6">
-                           با ما خودت رو به روز کن
-                           </div>
-                       </div>
-                       <div className="w-[90%] h-[64px] border absolute top-[158px] right-[55px] ">
-
-                       </div>
-                   </div>
-                   
-                      <img className="w-[45%] h-[490px] relative bottom-[46px] right-[25px] " src={images.Student} alt="" />
-                      
-               </div>
-          </div>
-
-
-          <div className="w-[100%] border-2 border-red-800 flex gap-7 ">
-             
-            <div id='left' className="border-2 border-blue-800 w-[20%] h-[1300px]  ml-auto ">
-                <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] ">
-                
-                   <div className=" w-[80%] h-16 mr-auto ml-auto border flex-col mt-4 ">
-                      
-                      <div className=" w-[100%] h-[50%] relative border pl-[23%] text-[20px] leading-[28px] font-bold text-[#005351] ">
-                      مطالب پیشنهادی 
-                      </div>
-                      <div className=" w-[100%] h-[50%] relative border pl-[10%] align-middle text-[13px] leading-[27px] font-normal text-[#AAAAAA] ">
-                      این مطالب هم میتونه براتون جالب باشه
-                      </div>
-                   </div>
-
-                   <div dir='rtl' className=" w-[85%] h-[320px] relative border flex flex-col mr-auto ml-auto  ">
-                      
-                      <div  className=" w-[85%] h-[70px] relative border flex flex-wrap">
-                          <div>
-
-                          </div>
-                          <div>
-                            
-                          </div>
-                          <img src={images.figma} alt="" />
-
-                      </div>
-                      <div>
-                          <img src="" alt="" />
-                          <div>
-
-                          </div>
-                          <div>
-                            
-                          </div>
-                      </div>
-                      <div>
-                          <img src="" alt="" />
-                          <div>
-
-                          </div>
-                          <div>
-                            
-                          </div>
-                      </div>
-                   </div>
+                <div className=" w-[80%] h-[50%] font-bold text-[35px] leading-[50px] text-[#00000] mr-3 ">
+                  اخبار و مقالات
                 </div>
 
-                <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] ">
-
+                <div className=" w-[60%] h-[50%] font-normal text-[20px] leading-[45px] text-[#AAAAAA] mr-6">
+                  با ما خودت رو به روز کن
                 </div>
-
-                <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] ">
-
-                </div>
+              </div>
+              <div className="w-[90%] h-[64px] border absolute top-[158px] right-[55px] "></div>
             </div>
-             
-              <div id='right' className="border-2 border-blue-800 w-[60%] h-[1280px] mr-auto flex-wrap ">
-                  
-                <div dir='rtl' className="w-[100%] border-2 flex gap-8  ">
-                    <div className=" w-[241px] h-[38px] border-pink-800 border-2 relative ">
 
-                    </div>
-                    <div className=" w-[207px] h-[38px] border-pink-800 border-2 relative ">
+            <img
+              className="w-[45%] h-[490px] relative bottom-[46px] right-[25px] "
+              src={images.Student}
+              alt=""
+            />
+          </div>
+        </div>
 
-                    </div>
+        <div className="w-[100%] border-2 border-red-800 flex gap-7 ">
+          <div
+            id="left"
+            className="border-2 border-blue-800 w-[20%] h-[1300px]  ml-auto "
+          >
+            <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] ">
+              <div className=" w-[80%] h-16 mr-auto ml-auto border flex-col mt-4 ">
+                <div className=" w-[100%] h-[50%] relative border pl-[23%] text-[20px] leading-[28px] font-bold text-[#005351] ">
+                  مطالب پیشنهادی
                 </div>
-
-                <div className="w-[100%] h-[1000px]">
-
-                  <div dir='rtl' className=" w-[291px] h-[381px] border bg-[#FFFFFF] rounded-t-[146px] shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)]  ">
-                     <img src={images.ReactA} alt="" className="w-[87%] h-[50%] rounded-[18px] mr-auto ml-auto mt-2 " />
-                    
-                    
-                                
-                    <div className="w-[87%] h-[50%] mr-auto ml-auto mt-[15px] "> 
-                     <div className=" h-[25px] w-[220px] text-[18px] leading-[27px] font-bold align-middle flex flex-nowrap text-[#003B39]">
-                       <p> انگولار بهتر است یا ری اکت؟ </p> 
-                     </div>
-                      
-                      
-
-                        <div className=" h-[61px] w-[100%]  ">
-                          <div className=" h-[100%] w-[105%]  m-auto text-[11px] leading-[16px] font-normal align-middle text-[#AAAAAA] mt-3"> 
-                          angular یا react کدام بهتر است؟ اگه شما تو حوزه طراحی و برنامه نویسی وب فعالیت داشته باشین، قطعا اسم تکنولوژی هایی مثل react و angular رو ...                           
-                          </div>
-                        </div>
-
-                      <div className=" h-[20px] w-[110%]  flex gap-[109px] ">
-
-                        <div className=" h-[15px] w-[65px] flex">
-
-                          <img src={images.Eyse} alt="" className=" h-[100%] w-[15px] " />
-
-                          <span className=" text-[12px] leading-[12px] font-normal text-[#888888] mr-1 mt-[2px] "> ۵۶ بازدید </span>
-
-                        </div>
-
-                        <div className=" h-[15px] w-[86px] flex">
-
-                          <span className=" text-[12px] leading-[12px] font-normal text-[#888888] ml-1 "> ۱۴۰۳/۰۱/۱۸ </span>
-                          <img src={images.Callender} alt="" className=" h-[15px] w-[15px]" />
-
-
-                        </div>
-                      </div>
-
-                      <div className=" h-[55px] w-[100%] flex  ">
-                          
-                          <button className=" h-[40px] w-[350px]  relative border-t border-[#F3F3F3] "> 
-                          <span className=" mr-8 mt-[px] text-[11px] leading-4 text-[#D47300] font-bold "> مطالعه بیشتر + </span>
-
-                          </button>
-
-                      </div>
-                    </div>         
-                    
-
-
-                  </div>
-
+                <div className=" w-[100%] h-[50%] relative border pl-[10%] align-middle text-[13px] leading-[27px] font-normal text-[#AAAAAA] ">
+                  این مطالب هم میتونه براتون جالب باشه
                 </div>
-
               </div>
 
+              <div
+                dir="rtl"
+                className=" w-[85%] h-[320px] relative border flex flex-col mr-auto ml-auto  "
+              >
+                <div className=" w-[85%] h-[70px] relative border flex flex-wrap">
+                  <div></div>
+                  <div></div>
+                  <img src={images.figma} alt="" />
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <div></div>
+                  <div></div>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
 
-              
+            <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] "></div>
+
+            <div className="w-[100%] h-[407px] bg-[#ffffff] rounded-[10px] mb-9 flex-wrap relative shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)] "></div>
           </div>
 
+          <div
+            id="right"
+            className="border-2 border-blue-800 w-[60%] h-[1280px] mr-auto flex-wrap "
+          >
+            <div dir="rtl" className="w-[100%] border-2 flex gap-8  ">
+              <div className=" w-[241px] h-[38px] border-pink-800 border-2 relative "></div>
+              <div className=" w-[207px] h-[38px] border-pink-800 border-2 relative "></div>
+            </div>
+
+            <div className="w-[100%] h-[1000px]">
+              <div
+                dir="rtl"
+                className=" w-[291px] h-[381px] border bg-[#FFFFFF] rounded-t-[146px] shadow-[0_1px_2px_0px_rgba(0,0,0,0.25)]  "
+              >
+                <img
+                  src={images.ReactA}
+                  alt=""
+                  className="w-[87%] h-[50%] rounded-[18px] mr-auto ml-auto mt-2 "
+                />
+
+                <div className="w-[87%] h-[50%] mr-auto ml-auto mt-[15px] ">
+                  <div className=" h-[25px] w-[220px] text-[18px] leading-[27px] font-bold align-middle flex flex-nowrap text-[#003B39]">
+                    <p> انگولار بهتر است یا ری اکت؟ </p>
+                  </div>
+
+                  <div className=" h-[61px] w-[100%]  ">
+                    <div className=" h-[100%] w-[105%]  m-auto text-[11px] leading-[16px] font-normal align-middle text-[#AAAAAA] mt-3">
+                      angular یا react کدام بهتر است؟ اگه شما تو حوزه طراحی و
+                      برنامه نویسی وب فعالیت داشته باشین، قطعا اسم تکنولوژی هایی
+                      مثل react و angular رو ... 
+                    </div>
+                  </div>
+
+                  <div className=" h-[20px] w-[110%]  flex gap-[109px] ">
+                    <div className=" h-[15px] w-[65px] flex">
+                      <img
+                        src={images.Eyse}
+                        alt=""
+                        className=" h-[100%] w-[15px] "
+                      />
+
+                      <span className=" text-[12px] leading-[12px] font-normal text-[#888888] mr-1 mt-[2px] ">
+                        {" "}
+                        ۵۶ بازدید{" "}
+                      </span>
+                    </div>
+
+                    <div className=" h-[15px] w-[86px] flex">
+                      <span className=" text-[12px] leading-[12px] font-normal text-[#888888] ml-1 ">
+                        {" "}
+                        ۱۴۰۳/۰۱/۱۸{" "}
+                      </span>
+                      <img
+                        src={images.Callender}
+                        alt=""
+                        className=" h-[15px] w-[15px]"
+                      />
+                    </div>
+                  </div>
+
+                  <div className=" h-[55px] w-[100%] flex  ">
+                    <button className=" h-[40px] w-[350px]  relative border-t border-[#F3F3F3] ">
+                      <span className=" mr-8 mt-[px] text-[11px] leading-4 text-[#D47300] font-bold ">
+                        {" "}
+                        مطالعه بیشتر +{" "}
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default News
+export default News;
